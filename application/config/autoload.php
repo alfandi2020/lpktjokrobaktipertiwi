7aa92379c540a3e02e47f5aa94a0318b0173b561
adpratama <adpratama.adp@gmail.com>
2023-11-21 09:19:56 +0700

    perbaiki bug autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'session', 'textlibrary');
+$autoload['libraries'] = array('database', 'session', 'TextLibrary');
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'language_helper');
+$autoload['helper'] = array('url', 'language_helper', 'device_helper');
 
 /*
 | -------------------------------------------------------------------
