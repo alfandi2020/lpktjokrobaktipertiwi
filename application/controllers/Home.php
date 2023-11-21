4e2e8493fbc5e0a5beca0e4c64a6a1348f7fd6a6
adpratama <adpratama.adp@gmail.com>
2023-11-21 09:36:20 +0700

    perbaiki bug autoload

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -8,8 +8,9 @@ class Home extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->helper(array('form', 'url', 'language_helper', 'device_helper', 'date'));
+        $this->load->helper(array('form', 'url', 'language_helper'));
         $this->load->library('textlibrary');
+        $this->load->helper('date');
 
         $this->load->model('M_Article');
         $this->load->model('M_Setting');
