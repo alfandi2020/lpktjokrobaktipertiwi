f26384f25618610144b88fcfee803f52c954dd69
alfandi2020 <pandibl4530@gmail.com>
2023-12-09 15:48:09 +0700

    fas

--- application/views/id/pages/v_faq.php
+++ application/views/id/pages/v_faq.php
@@ -31,55 +31,55 @@
                                 <div class="acod-head">
                                     <h6 class="acod-title">
                                         <a href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#faq1" class="collapsed" aria-expanded="true">
-                                            1. Web design aorem apsum dolor sit amet?</a>
+                                        Belum bisa Bahasa sama sekali apakah bisa kak ?</a>
                                     </h6>
                                 </div>
                                 <div id="faq1" class="acod-body collapse" data-bs-parent="#accordion1">
-                                    <div class="acod-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+                                    <div class="acod-content">Bisa dong kak, kita pembelajaran dari 0 ya kak.</div>
                                 </div>
                             </div>
                             <div class="panel">
                                 <div class="acod-head">
                                     <h6 class="acod-title">
                                         <a href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#faq2" class="collapsed" aria-expanded="true">
-                                            2. Graphic design aorem apsum dolor ?</a>
+                                        Bagaimana cara pendaftaran magang ke Jepang ?</a>
                                     </h6>
                                 </div>
                                 <div id="faq2" class="acod-body collapse" data-bs-parent="#accordion1">
-                                    <div class="acod-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+                                    <div class="acod-content"><a href="https://lpktjokrobaktipertiwi.id/registrasi"> Disini</a> Langsung Pendaftaran online ya kak, jangan lupa setelah mengisi form masuk ke GRUB WA ya kak, Link sudah ada di Formulir tinggal klik saja .</div>
                                 </div>
                             </div>
                             <div class="panel">
                                 <div class="acod-head">
                                     <h6 class="acod-title">
                                         <a href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#faq3" class="collapsed" aria-expanded="true">
-                                            3. Developement aorem apsum dolor sit amet ? </a>
+                                        Ada Riwayat sakit asma apakah bisa kak ? </a>
                                     </h6>
                                 </div>
                                 <div id="faq3" class="acod-body collapse" data-bs-parent="#accordion1">
-                                    <div class="acod-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+                                    <div class="acod-content">Maaf kak tidak bisa, bila sudah sembuh total baru bisa ya kak.</div>
                                 </div>
                             </div>
                             <div class="panel">
                                 <div class="acod-head">
                                     <h6 class="acod-title">
                                         <a href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#faq4" class="collapsed" aria-expanded="true">
-                                            4. True Responsiveness consectetuer adipiscing ? </a>
+                                        Document yang dibawa apa saja ya kak Ketika pendaftaran  ? </a>
                                     </h6>
                                 </div>
                                 <div id="faq4" class="acod-body collapse" data-bs-parent="#accordion1">
-                                    <div class="acod-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+                                    <div class="acod-content">Perlengkapan yg harus dibawa: 1. Formulir Pendaftaran 2. Alat tulis 3. Masker 4. Photo background merah kemeja putih berdasi 4x6 dan 3x4 (@2lmbr) yg Uk 4 X 6 ditempel di formulir pendaftaran 7. Fc ktp,akte,kk,ijazah sd-terakhir @1 lmbr 8. Biaya 200rb</div>
                                 </div>
                             </div>
                             <div class="panel">
                                 <div class="acod-head">
                                     <h6 class="acod-title">
                                         <a href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#faq5" class="collapsed" aria-expanded="true">
-                                            5. Claritas est etiam processus ?</a>
+                                            Bidang kerjanya apa saja ya kak ?</a>
                                     </h6>
                                 </div>
                                 <div id="faq5" class="acod-body collapse" data-bs-parent="#accordion1">
-                                    <div class="acod-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+                                    <div class="acod-content">DAFTAR BIDANG KERJA LPK CMI Job Cowok operator mesin, pengelasan, pengepresan, pengecatan, pemipaan, buat cakar ayam, scaffolding, cor beton, cor logam dll Job Cewek garment, pengolahan makanan, pabrik kain, pabrik roti, care giver, dll.</div>
                                 </div>
                             </div>
                         </div>
@@ -90,7 +90,7 @@
         </div>
         <!-- Your Faq End -->
         <!-- Your Faq -->
-        <div class="section-full bg-gray content-inner">
+        <!-- <div class="section-full bg-gray content-inner">
             <div class="container">
                 <div class="section-head text-black text-left">
                     <h3>General Questions</h3>
@@ -156,9 +156,7 @@
                         </div>
                     </div>
                 </div>
-                <!-- Faq Info END -->
             </div>
-        </div>
-        <!-- Your Faq End -->
+        </div> -->
     </div>
 </div>
