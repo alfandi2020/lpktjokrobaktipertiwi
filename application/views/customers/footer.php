b063df3e2eef8a4926c587316bd558e139ab22b2
alfandi2020 <pandibl4530@gmail.com>
2024-02-25 18:06:59 +0700

    update customers

---
+++ application/views/customers/footer.php
@@ -0,0 +1,144 @@
+<!-- Footer -->
+<footer class="site-footer text-white">
+    <div class="footer-top bg-primary">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-5 col-xl-2 col-lg-2 col-sm-6 footer-col-4">
+                    <div class="widget widget_services border-0">
+                        <h5 class="m-b30 text-white">Company</h5>
+                        <ul>
+                            <li><a href="about-1.html">About Us</a></li>
+                            <li><a href="blog-grid-2.html">Blog</a></li>
+                            <li><a href="services-1.html">Services</a></li>
+                            <li><a href="privacy-policy.html">Privacy Policy</a></li>
+                            <li><a href="portfolio-details.html">Projects </a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 col-7 col-xl-2 col-lg-3 col-sm-6 footer-col-4">
+                    <div class="widget widget_services border-0">
+                        <h5 class="m-b30 text-white">Useful Link</h5>
+                        <ul>
+                            <li><a href="help-desk.html">Help Desk </a></li>
+                            <li><a href="shop.html">Shop</a></li>
+                            <li><a href="contact-1.html">Contact</a></li>
+                            <li><a href="portfolio-details.html">Portfolio</a></li>
+                            <li><a href="team-1.html">Team</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xl-4 col-lg-3 col-sm-6 footer-col-4">
+                    <div class="widget widget_getintuch">
+                        <h5 class="m-b30 text-white ">Contact us</h5>
+                        <ul>
+                            <li><i class="ti-location-pin"></i><strong>address</strong> demo address #8901 Marmora Road
+                                Chi Minh City, Vietnam </li>
+                            <li><i class="ti-mobile"></i><strong>phone</strong>0800-123456 (24/7 Support Line)</li>
+                            <li><i class="ti-email"></i><strong>email</strong>info@example.com</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-12 col-xl-4 col-lg-4 col-sm-6 footer-col-4 ">
+                    <div class="widget">
+                        <h5 class="m-b30 text-white">Subscribe To Our Newsletter</h5>
+                        <p class="text-capitalize m-b20">Lorem Ipsum is simply dummy text of the printing and
+                            typesetting industry has been the industry's standard dummy text ever since the..</p>
+                        <div class="subscribe-form m-b20">
+                            <form class="dzSubscribe" action="script/mailchamp.php" method="post">
+                                <div class="dzSubscribeMsg"></div>
+                                <div class="input-group">
+                                    <input name="dzEmail" required="required" class="form-control"
+                                        placeholder="Your Email Id" type="email">
+                                    <span class="input-group-btn">
+                                        <button name="submit" value="Submit" type="submit"
+                                            class="site-button black btnhover21 radius-xl">Subscribe</button>
+                                    </span>
+                                </div>
+                            </form>
+                        </div>
+                        <ul class="list-inline m-a0">
+                            <li><a href="javascript:void(0);" class="site-button btnhover21 facebook circle "><i
+                                        class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="javascript:void(0);" class="site-button btnhover21 google-plus circle "><i
+                                        class="fab fa-google-plus-g"></i></a></li>
+                            <li><a href="javascript:void(0);" class="site-button btnhover21 linkedin circle "><i
+                                        class="fab fa-linkedin-in"></i></a></li>
+                            <li><a href="javascript:void(0);" class="site-button btnhover21 instagram circle "><i
+                                        class="fab fa-instagram"></i></a></li>
+                            <li><a href="javascript:void(0);" class="site-button btnhover21 twitter circle "><i
+                                        class="fab fa-twitter"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- footer bottom part -->
+    <div class="footer-bottom  bg-primary">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 text-left "> <span>Copyright © 2022 <a href="https://dexignzone.com/"
+                            target="_blank">DexignZone</a></span> </div>
+                <div class="col-md-6 col-sm-6 text-right ">
+                    <div class="widget-link ">
+                        <ul>
+                            <li><a href="about-2.html"> About</a></li>
+                            <li><a href="help-desk.html"> Help Desk</a></li>
+                            <li><a href="privacy-policy.html"> Privacy Policy</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer END -->
+<button class="scroltop style3" type="button"><i class="fas fa-arrow-up"></i></button>
+</div>
+<!-- JAVASCRIPT FILES ========================================= -->
+<script src="<?= base_url() ?>assets/asset_fr/js/jquery.min.js"></script><!-- JQUERY.MIN JS -->
+<script src="<?= base_url() ?>assets/asset_fr/plugins/wow/wow.js"></script><!-- WOW JS -->
+
+<script src="<?= base_url() ?>assets/asset_fr/plugins/bootstrap/js/bootstrap.bundle.min.js"></script><!-- BOOTSTRAP.MIN JS -->
+<script src="<?= base_url() ?>assets/asset_fr/plugins/bootstrap-select/bootstrap-select.min.js"></script><!-- FORM JS -->
+<script src="<?= base_url() ?>assets/asset_fr/plugins/bootstrap-touchspin/jquery.bootstrap-touchspin.js"></script><!-- FORM JS -->
+<script src="<?= base_url() ?>assets/asset_fr/plugins/magnific-popup/magnific-popup.js"></script><!-- MAGNIFIC POPUP JS -->
+<script src="<?= base_url() ?>assets/asset_fr/plugins/counter/waypoints-min.js"></script><!-- WAYPOINTS JS -->
+<script src="<?= base_url() ?>assets/asset_fr/plugins/counter/counterup.min.js"></script><!-- COUNTERUP JS -->
+<script src="<?= base_url() ?>assets/asset_fr/plugins/imagesloaded/imagesloaded.js"></script><!-- IMAGESLOADED -->
+<script src="<?= base_url() ?>assets/asset_fr/plugins/masonry/masonry-3.1.4.js"></script><!-- MASONRY -->
+<script src="<?= base_url() ?>assets/asset_fr/plugins/masonry/masonry.filter.js"></script><!-- MASONRY -->
+<script src="<?= base_url() ?>assets/asset_fr/plugins/owl-carousel/owl.carousel.js"></script><!-- OWL SLIDER -->
+<script src="<?= base_url() ?>assets/asset_fr/plugins/lightgallery/js/lightgallery-all.min.js"></script><!-- Lightgallery -->
+<script src="<?= base_url() ?>assets/asset_fr/plugins/scroll/scrollbar.min.js"></script><!-- scroll -->
+<script src="<?= base_url() ?>assets/asset_fr/js/custom.js"></script><!-- CUSTOM FUCTIONS  -->
+<script src="<?= base_url() ?>assets/asset_fr/js/dz.carousel.min.js"></script><!-- SORTCODE FUCTIONS  -->
+<script src="<?= base_url() ?>assets/asset_fr/plugins/countdown/jquery.countdown.js"></script><!-- COUNTDOWN FUCTIONS  -->
+<script src="<?= base_url() ?>assets/asset_fr/js/dz.ajax.js"></script><!-- CONTACT JS  -->
+<script src="<?= base_url() ?>assets/asset_fr/plugins/rangeslider/rangeslider.js"></script><!-- Rangeslider -->
+
+<script src="<?= base_url() ?>assets/asset_fr/js/jquery.lazy.min.js"></script>
+<!-- REVOLUTION JS FILES -->
+<script src="<?= base_url() ?>assets/asset_fr/plugins/revolution/revolution/js/jquery.themepunch.tools.min.js"></script>
+<script src="<?= base_url() ?>assets/asset_fr/plugins/revolution/revolution/js/jquery.themepunch.revolution.min.js"></script>
+<!-- Slider revolution 5.0 Extensions  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->
+<script src="<?= base_url() ?>assets/asset_fr/plugins/revolution/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+<script src="<?= base_url() ?>assets/asset_fr/plugins/revolution/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+<script src="<?= base_url() ?>assets/asset_fr/plugins/revolution/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="<?= base_url() ?>assets/asset_fr/plugins/revolution/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="<?= base_url() ?>assets/asset_fr/plugins/revolution/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+<script src="<?= base_url() ?>assets/asset_fr/plugins/revolution/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+<script src="<?= base_url() ?>assets/asset_fr/plugins/revolution/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="<?= base_url() ?>assets/asset_fr/plugins/revolution/revolution/js/extensions/revolution.extension.video.min.js"></script>
+<script src="<?= base_url() ?>assets/asset_fr/js/rev.slider.js"></script>
+<script>
+jQuery(document).ready(function() {
+    'use strict';
+    dz_rev_slider_12();	
+    $('.lazy').Lazy();
+});	/*ready*/
+</script>
+
+</body>
+
+</html>
