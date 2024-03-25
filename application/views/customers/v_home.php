a4733e34245d99b52ee0cdf05260607f44b0d92e
adpratama <adpratama.adp@gmail.com>
2024-03-25 09:53:45 +0700

    update: alur program ytp di halaman customer

--- application/views/customers/v_home.php
+++ application/views/customers/v_home.php
@@ -301,7 +301,7 @@
                     <h2 class="title"><?= $lang['alur_program_title'] ?>
                     </h2>
                 </div>
-                <div class="col-md-4 col-10 text-center">
+                <!-- <div class="col-md-4 col-10 text-center">
 
                     <h5><?= $lang['alur_program_title_description']  ?></h5>
                     <div class="icon-bx-wraper service-box style5 shadow fly-box wow fadeInUp">
@@ -333,125 +333,125 @@
                     </div>
                 </div>
 
-            </div>
-            <div class="row justify-content-center">
+            </div> -->
+                <div class="row justify-content-center">
 
-                <div class="col-md-4 col-10 text-center">
-                    <h5><?= $lang['alur_program_title_description']  ?></h5>
-                    <div class="icon-bx-wraper service-box style5 shadow fly-box wow fadeInUp">
+                    <div class="col-md-4 col-10 text-center">
+                        <h5><?= $lang['alur_program_title_description']  ?></h5>
+                        <div class="icon-bx-wraper service-box style5 shadow fly-box wow fadeInUp">
+                            <?php
+                            if ($language == "" || $language == "id") {
+                            ?>
+                                <img src="<?= base_url() ?>assets/asset_fr/ALUR_PROGRAM_PEMAGANGAN_KE_JEPANG_ID.svg" alt="">
+                            <?php } else if ($language == "jp") { ?>
+                                <img src="<?= base_url() ?>assets/asset_fr/ALUR_PROGRAM_PEMAGANGAN_KE_JEPANG_JP.svg" alt="">
+                            <?php } else if ($language == "en") { ?>
+                                <img src="<?= base_url() ?>assets/asset_fr/ALUR_PROGRAM_PEMAGANGAN_KE_JEPANG_EN.svg" alt="">
+
+                            <?php } ?>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-10 text-center">
+                        <h5><?= $lang['alur_program_kemampuan_khusus']  ?></h5>
+                        <div class="icon-bx-wraper service-box style5 shadow fly-box wow fadeInUp">
+                            <?php
+                            if ($language == "" || $language == "id") {
+                            ?>
+                                <img src="<?= base_url() ?>assets/asset_fr/ALUR_PROGRAM_TOKUTEI_GINOU_KE_JEPANG_ID.svg" alt="">
+                            <?php } else if ($language == "jp") { ?>
+                                <img src="<?= base_url() ?>assets/asset_fr/ALUR_PROGRAM_TOKUTEI_GINOU_KE_JEPANG_JP.svg" alt="">
+                            <?php } else if ($language == "en") { ?>
+                                <img src="<?= base_url() ?>assets/asset_fr/ALUR_PROGRAM_TOKUTEI_GINOU_KE_JEPANG_EN.svg" alt="">
+
+                            <?php } ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center mt-3">
+
+                    <div class="col-10 text-center">
                         <?php
                         if ($language == "" || $language == "id") {
                         ?>
-                            <img src="<?= base_url() ?>assets/asset_fr/ALUR_PROGRAM_PEMAGANGAN_KE_JEPANG_ID.svg" alt="">
-                        <?php } else if ($language == "jp") { ?>
-                            <img src="<?= base_url() ?>assets/asset_fr/ALUR_PROGRAM_PEMAGANGAN_KE_JEPANG_JP.svg" alt="">
-                        <?php } else if ($language == "en") { ?>
-                            <img src="<?= base_url() ?>assets/asset_fr/ALUR_PROGRAM_PEMAGANGAN_KE_JEPANG_EN.svg" alt="">
-
-                        <?php } ?>
-                    </div>
-                </div>
-                <div class="col-md-4 col-10 text-center">
-                    <h5><?= $lang['alur_program_kemampuan_khusus']  ?></h5>
-                    <div class="icon-bx-wraper service-box style5 shadow fly-box wow fadeInUp">
+                            <p>* Setelah permintaan diterima, pada dasarnya membutuhkan 6 bulan untuk pengiriman peserta ke Jepang</p>
                         <?php
-                        if ($language == "" || $language == "id") {
+                        } else if ($language == "jp") {
                         ?>
-                            <img src="<?= base_url() ?>assets/asset_fr/ALUR_PROGRAM_TOKUTEI_GINOU_KE_JEPANG_ID.svg" alt="">
-                        <?php } else if ($language == "jp") { ?>
-                            <img src="<?= base_url() ?>assets/asset_fr/ALUR_PROGRAM_TOKUTEI_GINOU_KE_JEPANG_JP.svg" alt="">
-                        <?php } else if ($language == "en") { ?>
-                            <img src="<?= base_url() ?>assets/asset_fr/ALUR_PROGRAM_TOKUTEI_GINOU_KE_JEPANG_EN.svg" alt="">
-
-                        <?php } ?>
+                            <p>※候補者募集要望をいただいた後、候補者を日本に派遣するまで、基本的6ヶ月かかります。</p>
+                        <?php
+                        } else if ($language == "en") {
+                        ?>
+                            <p>* Once the request is received, it basically takes 6 months for participants to be sent to Japan</p>
+                        <?php
+                        } ?>
                     </div>
                 </div>
             </div>
-            <div class="row justify-content-center mt-3">
+        </div>
+        <!-- flow end -->
 
-                <div class="col-10 text-center">
+        <!-- Gallery -->
+        <div class="section-full content-inner bg-white">
+            <div class="container-fluid">
+                <div class="section-head style2 text-center">
+                    <h2 class="title"><?= $lang['foto_foto_ojt_text'] ?></h2>
+                </div>
+                <div class="gallery" id="gallery">
+
                     <?php
-                    if ($language == "" || $language == "id") {
+                    foreach ($galleries as $g) {
                     ?>
-                        <p>* Setelah permintaan diterima, pada dasarnya membutuhkan 6 bulan untuk pengiriman peserta ke Jepang</p>
-                    <?php
-                    } else if ($language == "jp") {
-                    ?>
-                        <p>※候補者募集要望をいただいた後、候補者を日本に派遣するまで、基本的6ヶ月かかります。</p>
-                    <?php
-                    } else if ($language == "en") {
-                    ?>
-                        <p>* Once the request is received, it basically takes 6 months for participants to be sent to Japan</p>
+                        <div class="col-12">
+                            <div class="content"><img src="<?= base_url($g->url_foto) ?>" alt=""></div>
+                        </div>
                     <?php
                     } ?>
                 </div>
             </div>
         </div>
     </div>
-    <!-- flow end -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.min.js" integrity="sha512-Y2IiVZeaBwXG1wSV7f13plqlmFOx8MdjuHyYFVoYzhyRr3nH/NMDjTBSswijzADdNzMyWNetbLMfOpIPl6Cv9g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.js" integrity="sha512-YibiFIKqwi6sZFfPm5HNHQYemJwFbyyYHjrr3UT+VobMt/YBo1kBxgui5RWc4C3B4RJMYCdCAJkbXHt+irKfSA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-    <!-- Gallery -->
-    <div class="section-full content-inner bg-white">
-        <div class="container-fluid">
-            <div class="section-head style2 text-center">
-                <h2 class="title"><?= $lang['foto_foto_ojt_text'] ?></h2>
-            </div>
-            <div class="gallery" id="gallery">
-
-                <?php
-                foreach ($galleries as $g) {
-                ?>
-                    <div class="col-12">
-                        <div class="content"><img src="<?= base_url($g->url_foto) ?>" alt=""></div>
-                    </div>
-                <?php
-                } ?>
-            </div>
-        </div>
-    </div>
-</div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.min.js" integrity="sha512-Y2IiVZeaBwXG1wSV7f13plqlmFOx8MdjuHyYFVoYzhyRr3nH/NMDjTBSswijzADdNzMyWNetbLMfOpIPl6Cv9g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.js" integrity="sha512-YibiFIKqwi6sZFfPm5HNHQYemJwFbyyYHjrr3UT+VobMt/YBo1kBxgui5RWc4C3B4RJMYCdCAJkbXHt+irKfSA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-<script>
-    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
-        event.preventDefault();
-        $(this).ekkoLightbox();
-    });
-
-    var gallery = document.querySelector('#gallery');
-    var getVal = function(elem, style) {
-        return parseInt(window.getComputedStyle(elem).getPropertyValue(style));
-    };
-    var getHeight = function(item) {
-        return item.querySelector('.content').getBoundingClientRect().height;
-    };
-    var resizeAll = function() {
-        var altura = getVal(gallery, 'grid-auto-rows');
-        var gap = getVal(gallery, 'grid-row-gap');
-        gallery.querySelectorAll('.gallery-item').forEach(function(item) {
-            var el = item;
-            el.style.gridRowEnd = "span " + Math.ceil((getHeight(item) + gap) / (altura + gap));
+    <script>
+        $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+            event.preventDefault();
+            $(this).ekkoLightbox();
         });
-    };
-    gallery.querySelectorAll('img').forEach(function(item) {
-        item.classList.add('byebye');
-        if (item.complete) {
-            console.log(item.src);
-        } else {
-            item.addEventListener('load', function() {
-                var altura = getVal(gallery, 'grid-auto-rows');
-                var gap = getVal(gallery, 'grid-row-gap');
-                var gitem = item.parentElement.parentElement;
-                gitem.style.gridRowEnd = "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
-                item.classList.remove('byebye');
+
+        var gallery = document.querySelector('#gallery');
+        var getVal = function(elem, style) {
+            return parseInt(window.getComputedStyle(elem).getPropertyValue(style));
+        };
+        var getHeight = function(item) {
+            return item.querySelector('.content').getBoundingClientRect().height;
+        };
+        var resizeAll = function() {
+            var altura = getVal(gallery, 'grid-auto-rows');
+            var gap = getVal(gallery, 'grid-row-gap');
+            gallery.querySelectorAll('.gallery-item').forEach(function(item) {
+                var el = item;
+                el.style.gridRowEnd = "span " + Math.ceil((getHeight(item) + gap) / (altura + gap));
             });
-        }
-    });
-    window.addEventListener('resize', resizeAll);
-    gallery.querySelectorAll('.gallery-item').forEach(function(item) {
-        item.addEventListener('click', function() {
-            item.classList.toggle('full');
+        };
+        gallery.querySelectorAll('img').forEach(function(item) {
+            item.classList.add('byebye');
+            if (item.complete) {
+                console.log(item.src);
+            } else {
+                item.addEventListener('load', function() {
+                    var altura = getVal(gallery, 'grid-auto-rows');
+                    var gap = getVal(gallery, 'grid-row-gap');
+                    var gitem = item.parentElement.parentElement;
+                    gitem.style.gridRowEnd = "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
+                    item.classList.remove('byebye');
+                });
+            }
         });
-    });
-</script>
+        window.addEventListener('resize', resizeAll);
+        gallery.querySelectorAll('.gallery-item').forEach(function(item) {
+            item.addEventListener('click', function() {
+                item.classList.toggle('full');
+            });
+        });
+    </script>
